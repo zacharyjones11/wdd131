7ee28179e7349f817a9003ff6510853f4ea15185
zacharyjones11 <zachary_jones11@byui.edu>
2025-02-12 16:48:20 -0700

    Updated javascript and css have been added to clean up the page

--- Blog/blog.js
+++ Blog/blog.js
@@ -1,27 +1,62 @@
 const articles = [
 	{
-		id: 1,
-		title: 'Septimus Heap Book One: Magyk',
-		date: 'July 5, 2022',
-		description:
-			'If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.',
-		imgSrc: 'https://upload.wikimedia.org/wikipedia/en/5/5f/Magkycover2.jpg',
-		imgAlt: 'Book cover for Septimus Heap 1',
-		ages: '10-14',
-		genre: 'Fantasy',
-		stars: '****'
+	id: 1,
+	title: "Septimus Heap Book One: Magyk",
+	date: "July 5, 2022",
+	description:
+	"If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.",
+	imgSrc: "https://upload.wikimedia.org/wikipedia/en/5/5f/Magkycover2.jpg",
+	imgAlt: "Book cover for Septimus Heap 1",
+	ages: "10-14",
+	genre: "Fantasy",
+	stars: "⭐⭐⭐⭐"
 	},
 	{
-		id: 2,
-		title: 'Magnus Chase Book One: Sword of Summer',
-		date: 'December 12, 2021',
-		description:
-			'The anticipated new novel by Rick Riordan. After Greek mythology (Percy Jackson), Greek/Roman (Heroes of Olympus), and Egyptian (Kane Chronicles), Rick decides to try his hand with Norse Mythology, and the end result is good.',
-		imgSrc:
-			'https://books.google.com/books/content/images/frontcover/xWuyBAAAQBAJ?fife=w300',
-		imgAlt: 'Book cover for Magnus Chase 1',
-		ages: '12-16',
-		genre: 'Fantasy',
-		stars: '⭐⭐⭐⭐'
+	id: 2,
+	title: "Magnus Chase Book One: Sword of Summer",
+	date: "December 12, 2021",
+	description:
+	"The anticipated new novel by Rick Riordan. After Greek mythology (Percy Jackson), Greek/Roman (Heroes of Olympus), and Egyptian (Kane Chronicles), Rick decides to try his hand with Norse Mythology, and the end result is good.",
+	imgSrc:
+	"https://books.google.com/books/content/images/frontcover/xWuyBAAAQBAJ?fife=w300",
+	imgAlt: "Book cover for Magnus Chase 1",
+	ages: "12-16",
+	genre: "Fantasy",
+	stars: "⭐⭐⭐⭐"
+	},
+	{
+	id: 3,
+	title: "Belgariad Book One: Pawn of Prophecy",
+	date: "Feb 12, 2022",
+	description:
+	'A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his "Aunt Pol" and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.',
+	imgSrc:
+	"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+	imgAlt: "Book cover for Pawn of Prophecy",
+	ages: "12-16",
+	genre: "Fantasy",
+	stars: "⭐⭐⭐⭐⭐"
 	}
-]
+];
+
+
+function renderArticles(article) {
+	return `
+	<div id="main-content">
+		<div>
+			<p>${article.date}</p>
+			<p>${article.ages}</p>
+			<p>${article.genre}</p>
+			<p>${article.stars}</p>
+		</div>
+		<section class="book-review">
+			<h2>${article.title}</h2>
+			<img src="${article.imgSrc}" alt="${article.imgAlt}" class="book-cover">
+			<h3>${article.description}</h3>
+		</section>
+	</div>`
+}
+
+const mainContent = document.querySelector('#main-content');
+
+mainContent.innerHTML = articles.map(renderArticles).join("");
